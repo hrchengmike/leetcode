3a1a286bb8f110955d225eb68d09a4862c7290f2
hrchengmike <16098148d@connect.polyu.hk>
2020-12-22 23:24:23 -0500

    230 kth smallest BST first commit, implemented tree methods, insert, print, delete whole tree

---
+++ 230_kth_smallest_BST/230_kth_smallest_BST.cpp
@@ -0,0 +1,88 @@
+//Definition for a binary tree node.
+#include <iostream>
+#include <deque>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        
+    }
+};
+
+//insert a node to the current BST 
+//return true if success
+bool insert_node( TreeNode* root, int key){
+    TreeNode* cur = root;
+    while (true){
+        if (key < cur->val){
+            if (cur->left == nullptr){
+                TreeNode* node = new TreeNode(key);
+                cur -> left = node;
+                return true;
+            }
+            cur = cur->left;
+        } else if (key > cur->val){
+            if (cur->right == nullptr){
+                TreeNode* node = new TreeNode(key);
+                cur -> right = node;
+                return true;
+            }
+            cur = cur->right;
+        } else {
+            cout << "The key value is already in the tree!\n";
+            return false;
+        }
+    }
+}
+
+void delete_tree (TreeNode* root){
+    deque <TreeNode*> q;
+    q.push_back(root);
+    while(!q.empty()){
+        TreeNode* cur = q.front();
+        q.pop_front();
+        if(cur->left!=nullptr) q.push_back(cur->left);
+        if(cur->right!=nullptr)q.push_back(cur->right);
+        delete cur;
+    }
+}
+
+void print_tree(TreeNode* node){
+    if(node->left != nullptr) print_tree(node->left);
+    cout << "node in tree: " << node->val << endl;
+    if(node->right != nullptr) print_tree(node->right);
+}
+
+TreeNode* min_node(TreeNode* root){
+    TreeNode* cur = root;
+    while(cur -> left!= nullptr){
+        cur = cur -> left;
+    }
+    cout << "min_node: "<< cur->val << endl;
+    return cur;
+}
+
+int main(){
+    //int keys [] = {5,3,6,2,4,1};
+    //int keys [] = {1,2,3,4,5,6};
+    int keys [] = {3,1,4,2};
+    TreeNode* root = new TreeNode(keys[0]);
+    for (int i = 1; i < sizeof(keys)/sizeof(*keys); i++){
+        cout << "insert: " << keys[i] <<endl;
+        insert_node(root, keys[i]);
+    }
+    print_tree(root);
+    min_node(root);
+    delete_tree(root);
+    return 0;
+}
