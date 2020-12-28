0cea58d5381684ab27e413970d50794e228d16cd
hrchengmike <16098148d@connect.polyu.hk>
2020-12-27 21:01:56 -0500

    finished kth min node in BST

--- 230_kth_smallest_BST/230_kth_smallest_BST.cpp
+++ 230_kth_smallest_BST/230_kth_smallest_BST.cpp
@@ -72,10 +72,35 @@ TreeNode* min_node(TreeNode* root){
     return cur;
 }
 
+void find_kth_min(TreeNode* cur, int* count, int* result, bool* finished, int k){
+    if(cur -> left != nullptr) find_kth_min(cur -> left, count, result, finished, k);
+    if(*finished == true) return;
+    if(*count == k){
+        *result = cur -> val;
+        *finished = true;
+        return;
+    } else {
+        (*count) ++;
+    }
+    if(cur -> right != nullptr) find_kth_min(cur -> right, count, result, finished, k);   
+}
+
+int kth_min(TreeNode* root, int k){
+    int* count = new int(1);
+    int* result = new int(1000);
+    bool* finished = new bool(false);
+    find_kth_min(root, count, result, finished, k);
+    int kth_min = *result;
+    delete count;
+    delete result;
+    delete finished;
+    return kth_min ;
+}
+
 int main(){
-    //int keys [] = {5,3,6,2,4,1};
+    int keys [] = {5,3,6,2,4,1};
     //int keys [] = {1,2,3,4,5,6};
-    int keys [] = {3,1,4,2};
+    //int keys [] = {3,1,4,2};
     TreeNode* root = new TreeNode(keys[0]);
     for (int i = 1; i < sizeof(keys)/sizeof(*keys); i++){
         cout << "insert: " << keys[i] <<endl;
@@ -83,6 +108,9 @@ int main(){
     }
     print_tree(root);
     min_node(root);
-    delete_tree(root);
+    //delete_tree(root);
+    for(int i = 1; i <= sizeof(keys)/sizeof(*keys); i++){
+        cout << i << " th min is: " << kth_min(root, i) << endl;
+    }
     return 0;
 }
