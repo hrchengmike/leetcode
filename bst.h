f80cd7dcdc00cbd0c25a5f568e29000876c50f9f
hrchengmike <16098148d@connect.polyu.hk>
2021-01-25 22:40:32 -0500

    forgot to include update in header file

--- bst.h
+++ bst.h
@@ -13,7 +13,3 @@ TreeNode* init_tree(int vals [], int length);
 bool insert_node( TreeNode* root, int key);
 void delete_tree (TreeNode* root);
 void print_tree(TreeNode* node);
-TreeNode* min_node(TreeNode* root);
-void find_kth_min(TreeNode* cur, int* count, int* result, bool* finished, int k);
-int kth_min(TreeNode* root, int k);
-int kth_min_iterative(TreeNode* root, int k);
